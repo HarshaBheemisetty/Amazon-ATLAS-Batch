de63c4682a55fd1103ff00d67c6e3bd763468f02
HarshaBheemisetty <harshabheemisetty408@gmail.com>
2025-06-06 17:33:09 +0530

    commit

---
+++ src/TASK003.java
@@ -0,0 +1,12 @@
+public class TASK003 {
+    public static int add(int a, int b)
+    {
+        int sum = a+b;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void main(String[ ] args) {
+        add(5, 10);
+    }
+}
