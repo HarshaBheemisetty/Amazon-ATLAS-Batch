3962287885dc3d8456c4a90c0f59e57c7dc172c9
HarshaBheemisetty <harshabheemisetty408@gmail.com>
2025-06-06 13:16:59 +0530

    Greater of 2 Numbers

---
+++ src/TASK007.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TASK007 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        System.out.println("The name that I entered is :" + name);
+
+        System.out.println("Enter your Password : ");
+        int pwd = sc. nextInt();
+        System.out.println("The password I entered is : " + pwd);
+
+
+    }
+
+}
