e22cf1c0b97b6eb2a8e51f9827039e25b2e87b41
HarshaBheemisetty <harshabheemisetty408@gmail.com>
2025-06-06 13:08:53 +0530

    Greater of 2 Numbers

---
+++ src/TASK009.java
@@ -0,0 +1,16 @@
+public class TASK009 {
+
+    public static void main(String[] args)
+    {
+        int num1 = 30;
+        int num2 = 50;
+        if (num1>num2)
+        {
+            System.out.println(num1 + " is greater than " + num2);
+        }
+        else {
+            System.out.println(num2 + " is greater than " +num1);
+        }
+    }
+
+}
